1e33a4faa0b0b823548fd1ecb16a50b2af9bd40f
ziiihye <126834139+ziiihye@users.noreply.github.com>
2023-03-18 19:26:55 +0900

    3/18

--- js/script.js
+++ js/script.js
@@ -14,19 +14,20 @@ $(document).ready(function(){
     })
 
     // 메뉴 hover하면 메뉴 box나오는 것
-    $(".menu>li").mouseover(function(){
-        $("nav").addClass("show_nav");
-        $(".nav_inner_box").css("display","block")
-        let idx = $(".menu>li").index(this);
-        $(".nav_box").eq(idx).css("display","block");
-        $(".nav_box").eq(idx).siblings().css("display","none")
-    }) 
+        $(".menu>li").mouseover(function(){
+            $("nav").addClass("show_nav");
+            $(".nav_inner_box").css("display","block")
+            let idx = $(".menu>li").index(this);
+            $(".menu>li").eq(idx).mouseover(function(){
+                $(".nav_box").eq(idx).css("display","block");
+                $(".nav_box").eq(idx).siblings().css("display","none");
+            })
+            $(".nav_box").eq(idx).mouseleave(function(){
+                $("nav").removeClass("show_nav")
+                $(".nav_inner_box").css("display","none")
+            })
+        })
 
-    // 메뉴 mouseleave하면 box 사라짐
-    $(".menu>li").mouseleave(function(){
-        $("nav").removeClass("show_nav");
-        $(".nav_box").css("display","none")
-    }) 
 
 
     // 메인 화면 슬라이더 swiper 사용
@@ -65,5 +66,48 @@ $(document).ready(function(){
         $(".swiper_play").css("display","none");
         return false;
     });
+
+        // 검색 슬라이더 swiper 사용
+        let search = new Swiper ('.swiper', {
+            sliderPerView:'1',
+            speed: 500,
+            crossEffect: { crossFade: true },
+            autoplay:true,
+            loop: true,
+        });
+
+
+    // 검색 실시간 리스트 누르면 아래 메뉴 나오게
+    $("#search_down").click(function(){
+        $(".hidden_search_list").css("display","block");
+        $("#search_down").css("display","none");
+        $("#search_up").css("display","block");
+    })
+    $("#search_up").click(function(){
+        $(".hidden_search_list").css("display","none");
+        $("#search_down").css("display","block");
+        $("#search_up").css("display","none");
+    })
+
+    
+    // 자주찾는 서비스 슬라이드
+    let service = new Swiper ('.service_slide', {
+        slidesPerView: 8,
+        spaceBetween: 1,
+        slidesPerGroup:8,
+        autoplay:false,
+        loop: false,
+        navigation : {
+            nextEl : '.service_next_arrow',
+            prevEl : '.service_prev_arrow', 
+        },
+        pagination : { 
+            el : '.service_dot',
+            clickable : true,
+        },
+    });
+    
+
+
 
 });
